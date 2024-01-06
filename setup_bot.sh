57b88f406639e5593194da8042aa667aa05a75ed
andrei.lobanovich <andrei.lobanovich@akvelon.com>
2024-01-06 10:35:24 +0100

    add auto deploy

---
+++ setup_bot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Установка пути к директории скрипта
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Создание виртуального окружения Python
+python3 -m venv "$SCRIPT_DIR/venv"
+
+# Активация виртуального окружения
+source "$SCRIPT_DIR/venv/bin/activate"
+
+# Установка зависимостей из файла requirements.txt
+pip install -r "$SCRIPT_DIR/requirements.txt"
+
+# Добавление задачи в crontab
+(crontab -l 2>/dev/null; echo "* * * * * cd $SCRIPT_DIR && $SCRIPT_DIR/venv/bin/python $SCRIPT_DIR/main.py >> $SCRIPT_DIR/log.txt 2>&1") | crontab -
+
+# Деактивация виртуального окружения
+deactivate
